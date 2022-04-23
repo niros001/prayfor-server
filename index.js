cb240ac8600dfa9805a96a69f511e6fcc7b65815
niros <niro655@gmail.com>
2022-04-23 17:07:26 +0300

    Fix routes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/api/auth', authRoutes)
-app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')))
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
